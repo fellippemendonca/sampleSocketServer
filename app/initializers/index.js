6bce444348e95fb05028a662687f91989df69465
fellippemendonca <fellippe.mendonca@gmail.com>
2018-03-22 16:42:10 -0300

    Config file relocated

--- app/initializers/index.js
+++ app/initializers/index.js
@@ -1,9 +1,10 @@
 'use strict';
 
+const address = require('../../config');
 const socketServer = require('../../lib/socketServer/socketServer');
 const onDataMethod = require('../../lib/onDataMethod/onDataMethod');
 
 
 module.exports = {
-  socketServer: socketServer(onDataMethod)
+  socketServer: socketServer(address.host, address.port, onDataMethod)
 };
